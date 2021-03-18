681c88999c642036ffd94d9f251faea7a00a938c
Hinet60613 <hinet60613@gmail.com>
2021-03-18 23:51:23 +0800

    add Paper and Card style to Cheki and ChekiList

--- src/components/ChekiList/index.js
+++ src/components/ChekiList/index.js
@@ -1,3 +1,4 @@
+import { Paper, Card, CardContent, makeStyles, Typography, Chip } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { withFirebase } from "../Firebase/context";
 
@@ -15,45 +16,66 @@ const TEST_DATA = [
 
 const ChekiListItem = (props) => {
     const { maid, maid_cafe, date, is_2shot, received } = props;
+    const useStyles = makeStyles({
+        root: {
+            margin: 2,
+            width: 275,
+            height: 425,
+        },
+        title: {
+            fontSize: 14,
+        },
+        pos: {
+            marginBottom: 12,
+        },
+    });
+    const classes = useStyles();
     return (
-        <li>
-            {
-                received ?
-                    <span>
-                        <del>
-                            {(is_2shot) ? '[2shot]' : ''}
-                            {maid.join(", ")} @ {maid_cafe} -- {date}
-                        </del>
-                    </span>
-                    :
-                    <span>
-                        {(is_2shot) ? '[2shot]' : ''}
-                        {maid.join(", ")} @ {maid_cafe} -- {date}
-                    </span>
-            }
-        </li >
+        <Card className={classes.root} elevation={received ? 1 : 4}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary">
+                    {date}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    {maid.join(", ")}
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                    {maid_cafe}
+                </Typography>
+                {is_2shot ? <Chip label="2shot" /> : ''}
+            </CardContent>
+        </Card>
     );
 }
 
-const ChekiList = ({ docs }) => (
-    <ul>
-        {
-            docs.map(doc => {
-                const { id, maid, maid_cafe, date, is_2shot, received } = doc;
-                return (
-                    <ChekiListItem
-                        key={id}
-                        maid={maid}
-                        maid_cafe={maid_cafe}
-                        date={date}
-                        is_2shot={is_2shot}
-                        received={received}
-                    />
-                );
-            })
+const ChekiList = ({ docs }) => {
+    const useStyles = makeStyles({
+        root: {
+            display: "flex",
+            flexWrap: "wrap",
         }
-    </ul>
-)
+    });
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            {
+                docs.map(doc => {
+                    const { id, maid, maid_cafe, date, is_2shot, received } = doc;
+                    return (
+                        <ChekiListItem
+                            key={id}
+                            maid={maid}
+                            maid_cafe={maid_cafe}
+                            date={date}
+                            is_2shot={is_2shot}
+                            received={received}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+}
 
 const ChekiPageBase = ({ firebase }) => {
     const LIMIT = 10;
@@ -72,9 +94,9 @@ const ChekiPageBase = ({ firebase }) => {
     }, [loading, query]);
 
     return (
-        <div>
+        <Paper>
             {loading ? 'loading...' : <ChekiList docs={docs} />}
-        </div>
+        </Paper>
     )
 }
 
