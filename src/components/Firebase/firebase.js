05bd6ce8ee2778edecf9554d1642ee3a5ee3bef6
Hinet60613 <hinet60613@gmail.com>
2021-03-22 20:20:13 +0800

    add firebase.js

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,15 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import config from '../../constants/config';
+
+class Firebase {
+    constructor(props) {
+        app.initializeApp(config);
+        this.GoogleAuthProvider = new app.auth.GoogleAuthProvider();
+        this.auth = app.auth();
+        this.firestore = app.firestore();
+    }
+}
+
+export default Firebase;
